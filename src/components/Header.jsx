af0d1e8c7cae378a9bd3624cf25ff9593238bdaf
Alex <Shtifman.alex@gmail.com>
2020-12-23 12:06:22 +0200

    test commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+	return <header className='header'>
+				<img src='https://theteamw.com/img/logo/amazon.png'/>
+ 			</header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+	return  <div className="content">
+		<div>
+			<img src="https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/339/45/339-45-401P/Mops-Janine-Sommer-Acrylic-Glass-Print.jpg"/>
+		</div>
+
+		<div>
+			ava+description
+		</div>
+
+		<div>
+			My Post
+		</div>
+
+		<div>
+			New post
+		</div>
+
+		<div>
+			Post1
+		</div>
+	</div>
+}
+
+export default Profile;
